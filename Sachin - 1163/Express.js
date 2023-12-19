cb81570bdd816f24596736083e3b094963adbc2a
sachin31344 <105095798+sachin31344@users.noreply.github.com>
2023-12-19 11:04:48 +0530

    Express.js 2111981163
    Resolving conflicts

--- Sachin - 1163/Express.js
+++ Sachin - 1163/Express.js
@@ -3,9 +3,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"Bootstrap.html"));
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+  res.sendFile(path.join(__dirname,"Bootstrap.html"));
 })
-app.listen(port,()=>{
-    console.log(`Example app listening on port ${port}`)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
